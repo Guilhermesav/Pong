e9e66628fec5a0ab67a5178dd826fdd8291763cb
Guilhermesav <54535484+Guilhermesav@users.noreply.github.com>
2023-05-26 14:36:47 -0300

    alterações de treino para bola.py

--- bola.py
+++ bola.py
@@ -32,9 +32,28 @@ class Bola:
         else:
             return 0
         
+    def updateTreino(self):
+        self.posx += self.speed*self.xFac
+        self.posy += self.speed*self.yFac
+
+        if self.posy <= 0 or self.posy >= HEIGHT:
+            self.yFac *= -1
+        if self.posx <= 0 and self.contato:
+            self.contato = 0
+            return 1
+        elif self.posx >= WIDTH and self.contato:
+            self.contato = 0
+            return -1
+        else:
+            return 0
+        
     def reset(self):
         self.posx = WIDTH//2
         self.posy = HEIGHT//2
+        self.xFac *= -1
+        self.contato = 1
+        
+    def resetTreino(self):
         self.xFac *= -1
         self.contato = 1
 
