744f9d35d5c26092c16f41e5fc8f9c0186121292
Guilhermesav <54535484+Guilhermesav@users.noreply.github.com>
2023-06-16 13:29:29 -0300

    rede neural + alg genetico, porem bugado

--- algGenet.py
+++ algGenet.py
@@ -1,61 +1,119 @@
 import pygad
 import numpy
+import pygame
 import random
 from palheta import Palheta
 from bola import Bola
+from redeneural import RN
+WIDTH, HEIGHT = 800, 600
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+tela = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+FPS = 60
 
-score = random.randint(0,100)
-#bola_posx = Bola.posx
-#bola_posy = Bola.posy
-#jog_posy = Palheta.posy
-#jog_posx = Palheta.posx
-function_inputs = [score]
-def fitness_func(ga_instance, solution, solution_idx):
-    fitness = function_inputs
-    return fitness
+class AlgGenet():
+    def __init__(self):
+        self.palhetas = []
 
-def montarAlgoritmoGenetico(num_generations,
-                              num_parents_mating,
-                              sol_per_pop,num_genes,
-                              init_range_low,
-                              init_range_high,
-                              parent_selection_type,
-                              keep_parents,
-                              crossover_type,
-                              mutation_type,
-                              mutation_percent_genes):
-    fitness_function = fitness_func
+    def criar_pop(self,tam_pop):
+        for x in range(tam_pop):
+            palheta = Palheta(20, 0, 10, 100, 7, WHITE,0,numpy.random.uniform(-1,1,3),numpy.random.uniform(-1,1,1))
+            self.palhetas.append(palheta)
+             
+    def select_pais(self):
+        parents = []
+        total = 0
+        for x in range(len(self.palhetas)):
+            total += self.palhetas[x].fitness
+        fitness_normalizado = [self.palhetas[x].fitness/total for x in range(len(self.palhetas))]
 
-    num_generations = 100
-    num_parents_mating = 4
-    sol_per_pop = 8
-    num_genes = len(function_inputs)
+        cumulative_fitness = []
+        start = 0
+        for norm_value in fitness_normalizado:
+            start+=norm_value
+            cumulative_fitness.append(start)
 
-    init_range_low = -2
-    init_range_high = 5
+        population_size = len(self.palhetas)
 
-    parent_selection_type = "sss"
-    keep_parents = 1
+        for count in range(population_size):
+            random_number = random.uniform(0, 1)
+            individual_number = 0
+            for score in cumulative_fitness:
+                if(random_number<=score):
+                    parents.append(self.palhetas[individual_number])
+                    break
+            individual_number+=1
+        return parents
+    
+    def crossover(self,pai,mae):
+        filho_ent = numpy.mean([pai.genom_ent,mae.genom_ent],axis=0)
+        filho_ext = numpy.mean([pai.genom_ext,mae.genom_ext],axis=0)
+        palheta = Palheta(20, 0, 10, 100, 7, WHITE,0,filho_ent,filho_ext)
+        return palheta
+    
+    def mutacao(self, palheta):
+        palheta.genom_ent = numpy.random.uniform(-1,1,3)
+        palheta.genom_ext = numpy.random.uniform(-1,1,1)    
+        
+    def jogo(self,bola,geracao):
+        reserva = self.palhetas.copy()
+        for palheta in self.palhetas:
+            rn = RN(3,6,1,palheta.genom_ent,palheta.genom_ext)
+            while True:
+                pygame.display.update()
+                tela.fill(BLACK)
+                palheta.display()
+                bola.display()
+                palheta.drawPlacar("Fitness: ", palheta.fitness, 100, 20, WHITE)
+                palheta.drawPlacar("Geração: ", geracao, 300, 20, WHITE)
+                palheta.drawPlacar("Individuo:  ", self.palhetas.index(palheta), 500, 20, WHITE)
+                
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        running = False
+                for jogador in self.palhetas:
+                    if pygame.Rect.colliderect(bola.getRect(), jogador.getRect()):
+                        palheta.fitness += 500
+                        bola.colisao()
+                
+              
+                palheta.segue_bola(rn,bola)
 
-    crossover_type = "single_point"
+                ponto = bola.updateTreino()
 
-    mutation_type = "random"
-    mutation_percent_genes = 10
+                if ponto == -1: #direita
+                    bola.resetTreino()
+                elif ponto == 1: #esquerda
+                    reserva.remove(palheta)
+                    bola.reset()
+                    break
 
-    ga_instance = pygad.GA(num_generations=num_generations,
-                        num_parents_mating=num_parents_mating,
-                        fitness_func=fitness_function,
-                        sol_per_pop=sol_per_pop,
-                        num_genes=num_genes,
-                        init_range_low=init_range_low,
-                        init_range_high=init_range_high,
-                        parent_selection_type=parent_selection_type,
-                        keep_parents=keep_parents,
-                        crossover_type=crossover_type,
-                        mutation_type=mutation_type,
-                        mutation_percent_genes=mutation_percent_genes)
+                pygame.display.update()
+                clock.tick(FPS)
+                palheta.fitness +=1
+            
+        
+    def treino(self):
+       bola = Bola(WIDTH // 2, HEIGHT // 2, 7, 10, WHITE)
+       self.criar_pop(20)
+       for x in range(50):
+           self.jogo(bola,x)
+           new_gen = []
+           top_percentual = int((20 * len(self.palhetas))/100)
+           self.palhetas.sort(key=lambda palheta: palheta.fitness,reverse= True)
+           new_gen = self.palhetas[:top_percentual]
+           while len(new_gen) < len(self.palhetas):
+               pais = self.select_pais()
+               filho = self.crossover(pais[0],pais[1])
+               if random.random() < 0.5:
+                  self.mutacao(filho)
+               new_gen.append(filho)
+           self.palhetas = new_gen
+       print(self.palhetas)
 
-    ga_instance.run()
-
-
-montarAlgoritmoGenetico(100,4,8,1,-2,5,"sss",1,"single_point","random",10)
+if __name__ == "__main__":
+    game = AlgGenet()
+    game.treino()    
+    pygame.quit()
--- redeneural.py
+++ redeneural.py
@@ -1,19 +1,26 @@
-import pygad
-import pygad.gann
 import numpy
 import palheta
 import bola
-jogador = palheta.Palheta()
-ball = bola.Bola()
-games = []
-game_inputs = numpy.array(jogador.posy,ball.posx,ball.posy)
-solucoes = ""
-num_inputs = game_inputs.shape[1]
+import pygad
+from pygad import gann
+from pygad.gann import GANN
 
-for game in games:
-    GANN_instance = pygad.gann.GANN(num_solutions=solucoes,
-                                num_neurons_input=num_inputs,
-                                num_neurons_hidden_layers=[6],
-                                num_neurons_output=1,   
-                                hidden_activations=["relu"],
-                                output_activation="softmax")
+class RN:
+    def __init__(self,num_inputs,num_hidden_lay,tam_output,peso1,peso2):
+        self.num_inputs = num_inputs
+        self.num_hidden_lay = num_hidden_lay
+        self.tam_output = tam_output
+        self.peso1 = peso1
+        self.peso2 = peso2
+            
+    def feed_foward(self, input):
+        hidden_lay = self.relu(numpy.dot(input, self.peso1))
+        tam_output = self.sigmoid(numpy.dot(hidden_lay, self.peso2))
+        print(tam_output)
+        return tam_output
+
+    def relu(self, x):
+        return max(0, x)
+
+    def sigmoid(self, x):
+        return 1 / (1 + numpy.exp(-x))
